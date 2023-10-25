2df15759b997028578b84496e985da5ddf5824aa
Alec DuBois <after.alec@gmail.com>
2023-10-25 01:43:13 -0700

    Sort Tailwind on save

--- lua/plugins/tailwind-sorter.lua
+++ lua/plugins/tailwind-sorter.lua
@@ -2,5 +2,11 @@ return {
   "laytan/tailwind-sorter.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
   build = "cd formatter && npm i && npm run build",
-  config = true,
+  config = function()
+    require("tailwind-sorter").setup({
+      on_save_enabled = true,
+      on_save_pattern = { "*.html" },
+      node_path = "node",
+    })
+  end,
 }
