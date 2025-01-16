5a9b7cc77be1244ee9830cf2f9f7dddaffa97f60
Alec DuBois <after.alec@gmail.com>
2025-01-15 18:21:12 -0800

    Set Up For PHP and Blade

--- lua/plugins/tailwind-sorter.lua
+++ lua/plugins/tailwind-sorter.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("tailwind-sorter").setup({
       on_save_enabled = true,
-      on_save_pattern = { "*.html", "*.svelte", "*.vue" },
+      on_save_pattern = { "*.html", "*.svelte", "*.vue", "*.blade.php" },
       node_path = "node",
     })
   end,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
       "bash",
       "html",
       "javascript",
@@ -24,6 +24,26 @@ return {
       "c_sharp",
       "php",
       "vue",
-    },
-  },
+      "blade",
+      "php_only",
+    })
+  end,
+  config = function(_, opts)
+    vim.filetype.add({
+      pattern = {
+        [".*%.blade%.php"] = "blade",
+      },
+    })
+
+    require("nvim-treesitter.configs").setup(opts)
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade",
+    }
+  end,
 }
